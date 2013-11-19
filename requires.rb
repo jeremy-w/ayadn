b0dbfa84cee863807af7888fc1f3635d51f3a0b3
Eric Dejonckheere <eric@aya.io>
2013-11-19 16:34:23 +0100

    requires filename fix

---
+++ requires.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+require 'rubygems'
+require 'uri'
+require 'net/http'
+require 'rest_client'
+require 'json'
+require 'io/console'
+require 'fileutils'
+require_relative "lib/lib-tools"
+require_relative "lib/lib-api"
+require_relative "lib/lib-view"
+require_relative "lib/lib-main"
+require_relative "token"
+winPlatforms = ['mswin', 'mingw', 'mingw_18', 'mingw_19', 'mingw_20', 'mingw32']
+case Gem::Platform.local.os
+when *winPlatforms
+	require 'win32console'
+end
