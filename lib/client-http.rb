fc268c1ca4526ca458860033f4c7d211d7d7f4f1
Eric Dejonckheere <eric@aya.io>
2013-12-27 01:40:56 +0100

    little improvements + special chars in search

--- lib/client-http.rb
+++ lib/client-http.rb
@@ -6,18 +6,19 @@ class AyaDN
 		# TESTING DIFFERENT WAYS
 		# TODO: DRY
 		def clientHTTP(action, target = nil)
-			uri = URI("#{@url}")
-			https = Net::HTTP.new(uri.host,uri.port)
-			https.use_ssl = true
-			https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 			case action
 			when "delete"
+				uri = URI("#{@url}")
+				https = Net::HTTP.new(uri.host,uri.port)
+				https.use_ssl = true
+				https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 				request = Net::HTTP::Delete.new(uri.path)
 				request["Authorization"] = "Bearer #{@token}"
 				request["Content-Type"] = "application/json"
 				response = https.request(request)
 			when "get"
-				uri = URI.parse("#{target}")
+				encoded_url = URI.encode("#{target}")
+				uri = URI.parse(encoded_url)
 				https = Net::HTTP.new(uri.host,uri.port)
 				https.use_ssl = true
 				https.verify_mode = OpenSSL::SSL::VERIFY_NONE
