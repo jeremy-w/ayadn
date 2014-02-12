7b562bceb82b75c75edd9ec7fb5c042c284781d2
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2014-02-11 21:58:50 -0500

    removes redundant requires from requires.rb

--- lib/ayadn/requires.rb
+++ lib/ayadn/requires.rb
@@ -5,10 +5,6 @@
   require "#{r}"
 end
 
-%w{authorize main list post get-api api debug endpoints client-http view view-object view-interactions view-channels skip tools files adn_files extend status colors help pinboard user-stream}.each do |r|
-  require_relative "lib/#{r}"
-end
-
 winPlatforms = ['mswin', 'mingw', 'mingw_18', 'mingw_19', 'mingw_20', 'mingw32']
 case Gem::Platform.local.os
 when *winPlatforms
